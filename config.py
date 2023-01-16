178bebf34da295d2b5bcac79c5fe95c2a337a701
Race Developer Members <88087547+Shivayt007@users.noreply.github.com>
2023-01-16 21:52:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     BOT_USE = True
-    BOT_TOKEN = '5108987521:AAFzEtFz2HkP4FBYsyKkA9xyCfuGHWXn4EQ'                         # from @botfather
+    BOT_TOKEN = '5459553797:AAHuI82K3DVF7klRzFdXMtnY6ex6C-9nsXU'                         # from @botfather
     APP_ID = 11221860                              # from https://my.telegram.org/apps
     API_HASH = '94b9d2bf87ba56465fd3d1925de3e0c7'                          # from https://my.telegram.org/apps
-    AUTH_USERS = [5650108471]
+    AUTH_USERS = [957158815]
